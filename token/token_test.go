2da0dcc16a172dce4c8b60588a520fd823622614
Harvey <harvey@.com>
2023-09-29 11:18:05 +0800

    init

--- token/token_test.go
+++ token/token_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestToken_Encode(t *testing.T) {
 	var err error
-	DefaultToken, err = New("tGKN6AqvXCTYG3RrhdgLpTpYRgg6YBvT", time.Hour*7)
+	DefaultToken, err = New("tG1N6AqvXCTYG3Rrh5gLpTpYRgg6Y0vT", time.Hour*7)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -20,11 +20,11 @@ func TestToken_Encode(t *testing.T) {
 
 func TestToken_Decode(t *testing.T) {
 	var err error
-	DefaultToken, err = New("tGKN6AqvXCTYG3RrhdgLpTpYRgg6YBvT", time.Hour*7)
+	DefaultToken, err = New("tG1N6AqvXCTYG3Rrh5gLpTpYRgg6Y0vT", time.Hour*7)
 	if err != nil {
 		t.Fatal(err)
 	}
-	s, err := DefaultToken.Decode("5YLKX35Hzvpu4w0YUcvBhTXzCTcKpUXu1muAPLFlaWy1pOfH4msXjc8OVgBU7U0cg")
+	s, err := DefaultToken.Decode("6J0gSdw2bErtOfRwVcTd5jz4MsoQYAoHuZi4lg43cXyhstGwFVBFLa0l2Gz0nFiiUuxIH")
 	if err != nil {
 		t.Fatal(err)
 	}
