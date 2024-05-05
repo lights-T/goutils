9f9023ab50e1595c153827d3eca044573b3222b1
Harvey <harvey@.com>
2024-05-05 09:59:50 +0800

    新增自定义log标准输出

--- logger/cmd.go
+++ logger/cmd.go
@@ -7,25 +7,56 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-//ForCmdAnfFile 写入cmd及文件
-func ForCmdAnfFile(logPath string, level logrus.Level) {
+//CmdOutput cmd标准输出
+func CmdOutput(level logrus.Level) {
 	if level == 0 {
 		level = logrus.InfoLevel
 	}
-	//设置输出样式
-	//自带的只有两种样式logrus.JSONFormatter{}和logrus.TextFormatter{}
 	logrus.SetFormatter(&logrus.TextFormatter{})
 	logrus.SetOutput(os.Stdout)
-	//设置output,默认为stderr,可以为任何io.Writer，比如文件*os.File
-	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	writers := []io.Writer{file, os.Stdout}
-	//同时写文件和屏幕
+	writers := []io.Writer{os.Stdout}
 	fileAndStdoutWriter := io.MultiWriter(writers...)
-	if err == nil {
-		logrus.SetOutput(fileAndStdoutWriter)
-	} else {
-		logrus.Infof("Failed to log to file. [%s]", logPath)
-	}
-	//设置最低loglevel
+	logrus.SetOutput(fileAndStdoutWriter)
 	logrus.SetLevel(level)
 }
+
+//CmdOutput cmd标准输出
+//func CmdOutput(req *CmdOutputReq) {
+//	logPath := req.LogPath
+//	level := req.Level
+//	saveMaxDay := req.SaveMaxDay
+//	isWriteFile := req.IsWriteFile
+//	if level == 0 {
+//		level = logrus.InfoLevel
+//	}
+//	if saveMaxDay == 0 {
+//		saveMaxDay = 365
+//	}
+//	logrus.SetFormatter(&logrus.TextFormatter{})
+//	logrus.SetOutput(os.Stdout)
+//	writers := []io.Writer{os.Stdout}
+//	if isWriteFile {
+//		//设置output,默认为stderr,可以为任何io.Writer，比如文件*os.File
+//		file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+//		fileInfo, err := file.Stat()
+//		if err != nil {
+//			logrus.Errorf("Failed to get file information. [%s]", err.Error())
+//			return
+//		}
+//		if time.Now().Unix()-fileInfo.ModTime().Unix() > saveMaxDay {
+//			if err = files.Remove(logPath); err != nil {
+//				logrus.Errorf("Failed to remove file. [%s]", err.Error())
+//				return
+//			}
+//			//删除后重新创建
+//			file, err = os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+//			if err != nil {
+//				logrus.Errorf("Failed to log to file. [%s]", logPath)
+//			}
+//		}
+//		writers = append(writers, file)
+//	}
+//	fileAndStdoutWriter := io.MultiWriter(writers...)
+//	logrus.SetOutput(fileAndStdoutWriter)
+//	logrus.SetLevel(level)
+//}
