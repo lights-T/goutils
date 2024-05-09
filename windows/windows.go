cdc8fdb6ae09516e3a098651f5b682a1b4e4fd7b
Harvey <harvey@.com>
2024-05-09 10:03:15 +0800

    增加显示/隐藏命令行方法

--- windows/windows.go
+++ windows/windows.go
@@ -3,6 +3,7 @@ package windows
 import (
 	"github.com/lxn/walk"
 	"gopkg.in/toast.v1"
+	"syscall"
 )
 
 func MessageBoxByErr(content string) {
@@ -27,4 +28,27 @@ func Notify(appID, title, message string) {
 		//},
 	}
 	_ = notification.Push()
+}
+
+type HandleCMDRsp struct {
+	GetConsoleWindows *syscall.LazyProc
+	ShowWindowAsync   *syscall.LazyProc
+	ConsoleHandle     uintptr
+	R2                uintptr
+}
+
+func HandleCMD() (rsp *HandleCMDRsp, err error) {
+	rsp = &HandleCMDRsp{}
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	user32 := syscall.NewLazyDLL("user32.dll")
+	// https://docs.microsoft.com/en-us/windows/console/getconsolewindow
+	rsp.GetConsoleWindows = kernel32.NewProc("GetConsoleWindow")
+	// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindowasync
+	rsp.ShowWindowAsync = user32.NewProc("ShowWindowAsync")
+	rsp.ConsoleHandle, rsp.R2, err = rsp.GetConsoleWindows.Call()
+	//使用返回值时，注意err是否为nil
+	if rsp.ConsoleHandle == 0 {
+		return rsp, err
+	}
+	return rsp, nil
 }
--- windows/windows_test.go
+++ windows/windows_test.go
@@ -5,3 +5,14 @@ import "testing"
 func Test_Notify(t *testing.T) {
 	Notify("Example App", "notification", "Some message about how important something is...")
 }
+
+func Test_HandleCMD(t *testing.T) {
+	rsp, err := HandleCMD()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	t.Logf("GetConsoleWindows: %+v", rsp.GetConsoleWindows)
+	t.Logf("ShowWindowAsync: %+v", rsp.ShowWindowAsync)
+	t.Logf("ConsoleHandle: %+v", rsp.ConsoleHandle)
+	t.Logf("R2: %+v", rsp.R2)
+}
