6c3d9fa65953bada7e76844fb5e32a01bae60df8
Harvey <harvey@.com>
2024-11-06 21:22:25 +0800

    优化更新xml文件方法

--- files/xml/xml.go
+++ files/xml/xml.go
@@ -25,14 +25,6 @@ func Create(xmlName string, v any) error {
 }
 
 func Update(xmlName string, v any) error {
-	// 读取XML文件
-	_, err := files.ReadFile(xmlName)
-	if err != nil {
-		return goutils.Errorf("读取XML文件失败: %s", err.Error())
-	}
-	if err = files.Remove(xmlName); err != nil {
-		return goutils.Errorf("删除XML文件失败: %s", err.Error())
-	}
 	// 重新序列化XML数据
 	updatedData, err := xml.MarshalIndent(v, "", "  ")
 	if err != nil {
@@ -40,7 +32,7 @@ func Update(xmlName string, v any) error {
 	}
 
 	// 写入修改后的XML数据
-	if err = files.WriteFileByOptional(xmlName, os.O_CREATE, xml.Header+string(updatedData)); err != nil {
+	if err = ioutil.WriteFile(xmlName, []byte(xml.Header+string(updatedData)), 0644); err != nil {
 		return goutils.Errorf("写入XML文件失败: %s", err.Error())
 	}
 	return nil
