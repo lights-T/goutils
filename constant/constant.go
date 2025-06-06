dda938759645d8a631c4d2a38b47b714640c2815
Harvey <harvey@.com>
2025-06-06 16:29:24 +0800

    增加ISO 8601时间格式

--- constant/constant.go
+++ constant/constant.go
@@ -2,14 +2,15 @@ package constant
 
 //时间相关
 const (
-	DateLayoutYear    = "2006"
-	DateLayoutMouth   = "2006-01"
-	DateLayout        = "2006-01-02"
-	DatetimeLayout    = "2006-01-02 15:04:05"
-	DatetimeLayoutNa  = "2006-01-02 15:04:05.999"
-	DefaultMinTime    = "1971-01-01 00:00:00"
-	DefaultMaxTime    = "2099-01-01 00:00:00"
-	DatetimeLayoutMin = "2006-01-02 15:04"
+	DateLayoutYear        = "2006"
+	DateLayoutMouth       = "2006-01"
+	DateLayout            = "2006-01-02"
+	DatetimeLayout        = "2006-01-02 15:04:05"
+	DatetimeLayoutNa      = "2006-01-02 15:04:05.999"
+	DefaultMinTime        = "1971-01-01 00:00:00"
+	DefaultMaxTime        = "2099-01-01 00:00:00"
+	DatetimeLayoutMin     = "2006-01-02 15:04"
+	DatetimeLayoutISO8601 = "2006-01-02T15:04:05Z"
 
 	DateDayTime  = int64(86400) //一天的秒
 	DateHourTime = int64(3600)  //一小时的秒
